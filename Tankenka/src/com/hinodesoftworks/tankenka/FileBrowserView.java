86beb4ea844dc5ab6b3e2e0903aa1187f6059cd9
Michael Mancuso <tarkenfire@fullsail.edu>
2013-10-28 16:15:47 -0400

    Initial Commit of Project 1.

---
+++ Tankenka/src/com/hinodesoftworks/tankenka/FileBrowserView.java
@@ -0,0 +1,25 @@
+package com.hinodesoftworks.tankenka;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FileBrowserView extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_file_browser_view);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.file_browser_view, menu);
+		return true;
+	}
+
+}
