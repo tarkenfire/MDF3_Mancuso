fc19ba5d46314693a91c05538416c1b22a00ae50
Michael Mancuso <tarkenfire@fullsail.edu>
2013-10-28 09:45:56 -0400

    Initial Commit - Project 1

---
+++ Ki/src/com/hinodesoftwork/ki/BrowserActivity.java
@@ -0,0 +1,25 @@
+package com.hinodesoftwork.ki;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BrowserActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_browser);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.browser, menu);
+		return true;
+	}
+
+}
