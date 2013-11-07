044ee2c79ed9ada6ac170de199a0537e949c970f
Michael Mancuso <tarkenfire@fullsail.edu>
2013-11-07 17:06:03 -0500

    Completed Media Player Functionality.

--- Ongaku/src/com/hinodesoftworks/ongaku/MusicPlayerActivity.java
+++ Ongaku/src/com/hinodesoftworks/ongaku/MusicPlayerActivity.java
@@ -9,24 +9,37 @@ package com.hinodesoftworks.ongaku;
 
 import java.io.File;
 
+import com.hinodesoftworks.ongaku.MusicService.MusicBinder;
+
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class MusicPlayerActivity.
  */
-public class MusicPlayerActivity extends Activity
+public class MusicPlayerActivity extends Activity implements OnClickListener, OnSeekBarChangeListener
 {
+	
+	//general vars
+	MusicService musicService;
+	boolean isServiceBound = false;
+	
 	//ui handles
 	ImageView albumArtView;
 	SeekBar progressBar;
@@ -64,10 +77,15 @@ public class MusicPlayerActivity extends Activity
 		playerPlayPauseButton = (ImageButton)findViewById(R.id.player_play_pause_button);
 		playerForwardButton = (ImageButton)findViewById(R.id.player_forward_button);
 		
+		//set handlers
+		playerPlayPauseButton.setOnClickListener(this);
+		playerBackButton.setOnClickListener(this);
+		playerForwardButton.setOnClickListener(this);
+		progressBar.setOnSeekBarChangeListener(this);
+		
 		//get file for music track to play
 		Intent i = this.getIntent();
 		File musicFile = (File) i.getSerializableExtra("file");
-		
 		
 		//get and set fields from track
 		MediaMetadataRetriever mmr = new MediaMetadataRetriever();
@@ -86,19 +104,93 @@ public class MusicPlayerActivity extends Activity
 			albumArtView.setImageDrawable(new BitmapDrawable(this.getResources() ,BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length)));
 		}
 		
-		
-		
-		
-		
-		
-		//TODO start music playing
-		
+		//service will be both started AND bound, to allow for
+		//leaving the activity while still running the service
+		//while still having a binder to access the service.
+		Intent serviceIntent = new Intent(this, MusicService.class);
+		this.startService(serviceIntent);
 		
 		//set action bar icon as backward navigation.
 		//TODO doesn't work.
 	    getActionBar().setDisplayHomeAsUpEnabled(true);		
 	}
+	
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		
+		Intent serviceIntent = new Intent(this, MusicService.class);
+		this.bindService(serviceIntent, serviceConnection, BIND_AUTO_CREATE);
+	}
+	
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		if (isServiceBound)
+		{
+			unbindService(serviceConnection);
+			isServiceBound = false;
+		}
+	}
+	
+	//on destroy may not always be called, but if it is, stop the service.
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		Intent serviceIntent = new Intent(this, MusicService.class);
+		this.stopService(serviceIntent);
+	}
+	
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+		case R.id.player_back_button:
+			musicService.seekBackwardFiveSeconds();
+			break;
+		
+		case R.id.player_play_pause_button:
+			if (musicService.isMusicPlaying())
+			{
+				musicService.pauseTrack();
+			}
+			else
+			{
+				musicService.playTrack();
+			}
+			break;
+			
+		case R.id.player_forward_button:
+			musicService.seekForwardFiveSeconds();
+			break;
+		}
+	}
+	
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser)
+	{
+		if (fromUser)
+			musicService.seekToPoint(progress);
+		
+	}
 
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar)
+	{
+		//do nothing
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar)
+	{
+		//do nothing
+	}
+	
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
 	 */
@@ -110,6 +202,53 @@ public class MusicPlayerActivity extends Activity
 		return true;
 	}
 	
+	//public methods
+	public void setCurrentDuration(int milSeconds)
+	{
+		currentTimeDisplay.setText(AudioFileArrayAdapter.getDurationStringFromMilSeconds(milSeconds));
+		progressBar.setProgress(milSeconds);
+	}
+	
+	public void onPlaybackStarted(int milSeconds)
+	{
+		totalTimeDisplay.setText(AudioFileArrayAdapter.getDurationStringFromMilSeconds(milSeconds));
+		progressBar.setMax(milSeconds);
+	}
+	
+	//private methods
+	private void onServiceBound()
+	{
+		Intent i = this.getIntent();
+		File musicFile = (File) i.getSerializableExtra("file");
+		musicService.setPlayerInstanceReference(this);
+		musicService.prepareMediaPlayer(musicFile.getAbsolutePath());
+	}
+	
+	//service connection class to connect with binder from service
+	
+	private ServiceConnection serviceConnection = new ServiceConnection()
+	{
+
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service)
+		{
+			MusicBinder binder = (MusicBinder)service;
+			musicService = binder.getService();
+			isServiceBound = true;
+			onServiceBound();
+			
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName className)
+		{
+			isServiceBound = false;	
+		}
+		
+	};
+
+
+
 	
 
 }
--- Ongaku/src/com/hinodesoftworks/ongaku/MusicService.java
+++ Ongaku/src/com/hinodesoftworks/ongaku/MusicService.java
@@ -9,17 +9,126 @@ package com.hinodesoftworks.ongaku;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+
+import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
 
-public class MusicService extends Service
+public class MusicService extends Service implements OnPreparedListener
 {
-
+	private final IBinder _binder = new MusicBinder();
+	protected MediaPlayer mediaPlayer;
+	MusicPlayerActivity _contextRef;
+	boolean isPlayerPrepared = false;
+	
+	Handler timeHandler = new Handler();
+	
+	//binder class for client activities
+	public class MusicBinder extends Binder
+	{
+		MusicService getService()
+		{
+			return MusicService.this;
+		}
+	}
+	
 	@Override
 	public IBinder onBind(Intent intent)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return _binder;
+	}
+
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public void onPrepared(MediaPlayer player)
+	{
+		isPlayerPrepared = true;
 	}
 	
-
+	//private methods and runnables
+	private Runnable musicTimerRunnable = new Runnable()
+	{
+		@Override
+		public void run()
+		{
+			if (mediaPlayer.isPlaying())
+			{
+				_contextRef.setCurrentDuration(mediaPlayer.getCurrentPosition());
+				timeHandler.postDelayed(this, 1000);
+			}
+		}
+	};
+	
+	
+	//public methods
+	public boolean prepareMediaPlayer(String path)
+	{
+		mediaPlayer = new MediaPlayer();
+		isPlayerPrepared = false;
+		
+		try
+		{
+			mediaPlayer.setDataSource(path);
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+		
+		mediaPlayer.setOnPreparedListener(this);
+		mediaPlayer.prepareAsync();
+		return true;
+	}
+	
+	public void playTrack()
+	{
+		if (isPlayerPrepared)
+		{
+			mediaPlayer.start();
+			_contextRef.onPlaybackStarted(mediaPlayer.getDuration());
+			timeHandler.postDelayed(musicTimerRunnable, 1000);
+		}
+	}
+	
+	public void pauseTrack()
+	{
+		if (mediaPlayer.isPlaying())
+			mediaPlayer.pause();
+	}
+	
+	public void seekToPoint(int milSeconds)
+	{
+		if (isPlayerPrepared)
+			mediaPlayer.seekTo(milSeconds);
+	}
+	
+	public void seekForwardFiveSeconds()
+	{
+		if (isPlayerPrepared)
+			mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 5000);
+	}
+	
+	public void seekBackwardFiveSeconds()
+	{
+		if (isPlayerPrepared)
+			mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 5000);
+	}
+	
+	public boolean isMusicPlaying() 
+	{
+		return mediaPlayer.isPlaying();
+	}
+	
+	public void setPlayerInstanceReference(MusicPlayerActivity activity)
+	{
+		_contextRef = activity;
+	}
 }
