831ceed4e74df1e09e4a63dc11f5a7103f9efa00
Michael Mancuso <tarkenfire@fullsail.edu>
2013-11-14 07:28:18 -0500

    Implemented Basic Widget

--- Ongaku/src/com/hinodesoftworks/ongaku/WidgetProvider.java
+++ Ongaku/src/com/hinodesoftworks/ongaku/WidgetProvider.java
@@ -1,8 +1,26 @@
 package com.hinodesoftworks.ongaku;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 public class WidgetProvider extends AppWidgetProvider
 {
+
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		// TODO Auto-generated method stub
+		super.onReceive(context, intent);
+	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds)
+	{
+		// TODO Auto-generated method stub
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
 
 }
